353ccf12971900dc14e29dd9673daa012420cea5
xih <yxing@berkeley.edu>
2018-03-13 17:10:10 -0700

    started working on SSENSE spider

---
+++ scraping/spiders/ssense.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider
+from scrapy.http import Request
+
+
+class SsenseSpider(Spider):
+    name = 'ssense'
+    allowed_domains = ['ssense.com']
+    start_urls = ['https://www.ssense.com/en-us/men', 'https://www.ssense.com/en-us/women']
+
+    def parse(self, response):
+         #exclude the first one because that contains ''/en-us/women'
+        relative_designer_urls = response.xpath('//*[@id="designer-list"]//a/@href').extract()[1:]
+        for relative_designer_url in relative_designer_urls:
+            absolute_designer_url = response.urljoin(relative_designer_url)
+            yield Request(absolute_designer_url, callback=self.parse_designer)
+
+    def parse_designer(self, response):
+        
